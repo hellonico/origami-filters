d7fb7528582040cafe779878e6ee760eac684088
Nicolas Modrzyk <hellonico@gmail.com>
2020-02-25 14:48:17 +0900

    sepia filter was missing

--- src/main/java/origami/filters/Sepia.java
+++ src/main/java/origami/filters/Sepia.java
@@ -2,24 +2,47 @@ package origami.filters;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import origami.Filter;
 
 import static org.opencv.core.Core.transform;
 
 /**
  * Using a kernel to get sepia picture
  */
-public class Sepia {
+public abstract class Sepia implements Filter {
+    public static class Gray extends Sepia {
+        public Gray() {
+            super();
+            kernel.put(0,0,
+                    // rgb -> blue
+                    0.272, 0.534, 0.131,
+                    // rgb -> green
+                    0.349, 0.686, 0.168,
+                    // rgb -> red
+                    0.393, 0.769, 0.189);
+        }
+    }
+
+    public static class Red extends Sepia {
+        public Red() {
+            super();
+            // mat is in BGR
+            kernel.put(0, 0,
+                    // green
+                    0.272, 0.534, 0.131,
+                    // blue
+                    0.349, 0.686, 0.168,
+                    // red
+                    0.593, 0.769, 0.489);
+        }
+    }
+
+    Mat kernel = new Mat(3, 3, CvType.CV_32F);
+    public Sepia() {
+
+    }
 
     public Mat apply(Mat source) {
-        // mat is in BGR
-        Mat kernel = new Mat(3, 3, CvType.CV_32F);
-        kernel.put(0, 0,
-                // green
-                0.272, 0.534, 0.131,
-                // blue
-                0.349, 0.686, 0.168,
-                // red
-                0.393, 0.769, 0.189);
         Mat destination = new Mat();
         transform(source, destination, kernel);
         return destination;
