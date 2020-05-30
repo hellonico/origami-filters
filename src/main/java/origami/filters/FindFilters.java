d4ab211466ec77aef97c52a498839998deb69d64
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-30 14:54:15 +0900

    start

---
+++ src/main/java/origami/filters/FindFilters.java
@@ -0,0 +1,32 @@
+package origami.filters;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindFilters {
+    public static List<String> findFilters() {
+        try (
+                // ScanResult scanResult = new
+                // ClassGraph().enableAllInfo().whitelistPackages("origami")
+                // .scan())
+                ScanResult scanResult = new ClassGraph().enableAllInfo().scan()) {
+            ClassInfoList widgetClasses = scanResult.getClassesImplementing("origami.Filter");
+            return widgetClasses.getNames().stream().filter(k -> {
+                try {
+                    return Class.forName(k).getConstructor() != null;
+                } catch (Exception e) {
+                    return false;
+                }
+            }).collect(Collectors.toList());
+        }
+    }
+
+    public static void main(String... args) {
+        for (String s : findFilters())
+            System.out.println(s);
+    }
+}
