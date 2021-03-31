d01d93e35a3223fd100672fd195b1ce00a2840ed
Nicolas Modrzyk <hellonico@gmail.com>
2021-03-31 17:14:30 +0900

    yolov4

--- src/main/java/origami/filters/MyYolo.java
+++ src/main/java/origami/filters/MyYolo.java
@@ -9,18 +9,18 @@ public class MyYolo {
         }
     }
     public static class V2Tiny extends Yolo {
-        public V2() {
+        public V2Tiny() {
             super("networks.yolo:yolov2-tiny:1.0.0");
         }
     }
     public static class V3 extends Yolo {
-        public V2() {
+        public V3() {
             super("networks.yolo:yolov3:1.0.0");
         }
     }
 
     public static class V3Tiny extends Yolo {
-        public V3() {
+        public V3Tiny() {
             super("networks.yolo:yolov3:1.0.0");
         }
     }
