386f3f1590fa41c80e28aceae2739392d3c7f260
Nicolas Modrzyk <hellonico@gmail.com>
2024-05-08 10:46:11 +0900

    hellonico/origami#134

--- src/main/java/origami/filters/brandnew/ShadowFilter.java
+++ src/main/java/origami/filters/brandnew/ShadowFilter.java
@@ -9,29 +9,57 @@ import origami.Filter;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.opencv.core.Core.*;
+import static org.opencv.imgproc.Imgproc.*;
+
 public class ShadowFilter implements Filter {
+
+    // Apply shading effect using a distant light source
+    public int lightSourceX = 100;  // X-coordinate of the light source (negative for distant light)
+    public int lightSourceY = 100;  // Y-coordinate of the light source (negative for distant light)
+    public double shadingFactor = 0.5;  // Shading factor between 0.0 and 1.0 (adjust as needed)
+
+    public int getLightSourceX() {
+        return lightSourceX;
+    }
+
+    public void setLightSourceX(int lightSourceX) {
+        this.lightSourceX = lightSourceX;
+    }
+
+    public int getLightSourceY() {
+        return lightSourceY;
+    }
+
+    public void setLightSourceY(int lightSourceY) {
+        this.lightSourceY = lightSourceY;
+    }
+
+    public double getShadingFactor() {
+        return shadingFactor;
+    }
+
+    public void setShadingFactor(double shadingFactor) {
+        this.shadingFactor = shadingFactor;
+    }
+
     @Override
     public Mat apply(Mat image) {
         // Convert the image to grayscale
         Mat grayImage = new Mat();
-        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-
-        // Apply shading effect using a distant light source
-        int lightSourceX = 100;  // X-coordinate of the light source (negative for distant light)
-        int lightSourceY = 100;  // Y-coordinate of the light source (negative for distant light)
-        double shadingFactor = 0.5;  // Shading factor between 0.0 and 1.0 (adjust as needed)
+        cvtColor(image, grayImage, COLOR_BGR2GRAY);
 
         Mat shadingImage = new Mat();
-        Core.subtract(grayImage, new Scalar(255 * shadingFactor), shadingImage);
+        subtract(grayImage, new Scalar(255 * shadingFactor), shadingImage);
 
         // Create a 3-channel image for merging the shaded image with the original color image
         Mat resultImage = new Mat();
 
         List<Mat> channels = Arrays.asList(shadingImage, shadingImage, shadingImage);
-        Core.merge(channels, resultImage);
+        merge(channels, resultImage);
 
         // Add the shaded image to the original color image
-        Core.add(resultImage, image, resultImage);
+        add(resultImage, image, resultImage);
         return resultImage;
 
     }
